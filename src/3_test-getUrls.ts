8c43964a7f9f4b7f144788e51918f55f3e50c1e5
maradondt <maradondt@gmail.com>
2024-06-26 12:07:27 +0200

    fix task description

--- src/3_test-getUrls.ts
+++ src/3_test-getUrls.ts
@@ -1,12 +1,12 @@
 /**
- * Implement the function that will query the URLs 
- * and return an array of responses ['url1_answer', 'url2_answer', ...] 
- * in the shortest possible time.
- * 
- * At any given time, no more than [limit] requests can be executed.
+ * Implement the function that will query the URLs
+ * and return an array of responses [ResponseDataType, ResponseDataType, ...].
+ * The solution should execute all requests in parallel
+ * but no more than [limit] requests can be executed concurrently.
+ * The goal is to minimize the total execution time.
+ *
  * Use the asyncFetch function as a replacement for fetch.
  */
-
 interface ResponseDataType {
   delay: number;
   data: string;
@@ -15,7 +15,7 @@ const asyncFetch = (url: string) =>
   new Promise<ResponseDataType>((resolve) => {
     const delay = Math.random() * 1000;
     setTimeout(() => {
-      resolve({ delay, data: "result" + url });
+      resolve({ delay, data: 'result' + url });
     }, delay);
   });
 
@@ -24,15 +24,10 @@ const urlsToFetch = Array(30)
   .map((fn, index) => fn(index));
 
 /**               Решение задачи                */
-const paralellUploading = (
-  urls: string[],
-  parallelLimit: number
-): Promise<ResponseDataType[]> => {
+const paralellUploading = (urls: string[], parallelLimit: number): Promise<ResponseDataType[]> => {
   /** START SOLUTION HERE */
 
   return Promise.resolve([]);
 };
 
-paralellUploading(urlsToFetch, 3).then((x) =>
-  console.log("3_test-getUrls.ts", x)
-);
+paralellUploading(urlsToFetch, 3).then((x) => console.log('3_test-getUrls.ts', x));
